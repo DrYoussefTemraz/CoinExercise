cf787cbb728ba0d5a2548713b2a8c6e4741c348f
DrYoussefTemraz <youseftahertemraz@gmail.com>
2023-12-19 03:28:59 +0200

    update

---
+++ src/Coin.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Coin extends Component {
+  render() {
+    return <img src={this.props.info.imgSrc} alt={this.props.info.side} />;
+  }
+}
